e9b22b56ca6a3761495f4231d94c61511854b6ee
codebycody <codebycody@protonmail.com>
2019-04-22 23:21:40 -0400

    issue with the global assinment to best_score
    Will look at the solution to persistance given by treehouse instructor

--- Project 1 Number Guessing Game/guessing_game.py
+++ Project 1 Number Guessing Game/guessing_game.py
@@ -4,7 +4,7 @@ def main():
 	
 	# Generate the random number between 1 and 10
 	random_number = random.randint(1,10)
-	best_score = None
+	best_score = 1
 	current_guesses = 0
 
 	def welcome_setup():
@@ -12,24 +12,24 @@ def main():
 		print('-------------------------------')
 		print(' Welcome to the guessing game.')
 		print('-------------------------------')
-		print(best_score)
 		# If rematch post the lowest number of guesses to win stored
 		if (best_score):
 			print('The HIGHSCORE is {}'.format(best_score))
 
-	def guess_loop(random_number, best_score, current_guesses):
+	def guess_loop(random_number, current_guesses):
 		while True:
 			try:
 				# Ask for guess of what the number is
 				guessed_number = int(input('Guess a number between 1 and 10: '))
-					# make sure the guess is a number between 1 and 10
-					# if it isn't a number or between 1 and 10 throw error and have player guess again
-					# is the number the same as the generated number
+				# make sure the guess is a number between 1 and 10
+				# if it isn't a number or between 1 and 10 throw error and have player guess again
+				# is the number the same as the generated number
 				if (guessed_number == random_number):
-					# Congradulate the player and tell them the number of tries it toke to win
+					# Congradulate the player and tell them the number of tries it took to win
 					print('Congrats you guessed the number! You got it in {} trys'.format(current_guesses + 1))
 					if (best_score == None or best_score > (current_guesses + 1 )):
 						best_score = (current_guesses + 1)
+					print('the best score is', best_score)
 					break
 				# is the number lower then the generated number
 				elif (guessed_number < random_number):
@@ -45,10 +45,10 @@ def main():
 		replay_game = input('Would you like to play again? [y]es/[n]o ')
 		if (replay_game.lower() == 'y'):
 			welcome_setup()
-			guess_loop(random.randint(1,10), best_score, current_guesses)
+			guess_loop(random.randint(1,10), current_guesses)
 
 	welcome_setup()
-	guess_loop(random.randint(1,10), best_score, current_guesses)
+	guess_loop(random.randint(1,10), current_guesses)
 
 if __name__ == '__main__':
     # Kick off the program by calling the main function.
