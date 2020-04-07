3fff83e8074a79c8eccfafe29b1970a0618862a1
codebycody <codebycody@protonmail.com>
2020-04-07 13:29:15 -0400

    update to app.py
    player data is now split evenly across number of teams.
    listing teams shows the number of players aswell as listing the names out.

--- Project 2 Basketball Stats Tool/app.py
+++ Project 2 Basketball Stats Tool/app.py
@@ -1,6 +1,18 @@
 import constants
 
 def main():
+
+	def input_validation(user_input, *args):
+		"""Checks user input against know valid options
+		If user_input in args, return True
+		If user_input not in args, return False with message
+		"""
+		if(user_input in args):
+			return True
+		else:
+			print('Please select a valid option')
+			return False
+		
 	def startup_message():
 		print('BASKETBALL TEAM STATS TOOL')
 		print('')
@@ -11,7 +23,7 @@ def main():
 		print('2) Quit')
 		print('')
 	
-	def menu_selection():
+	def menu_selection(*args):
 		selection = input('Enter an option > ')
 		print('')
 		return selection
@@ -21,32 +33,42 @@ def main():
 		print('2) Bandits')
 		print('3) Warriors')
 		print('')
+		list_team(menu_selection(1, 2, 3))
 
-	def list_team():
-		print('Team: Panthers Stats')
+	def list_team(selection):
+		team_name = team_names[int(selection)-1]
+		total_players = len(team_roster[int(selection)-1][team_name])
+		print('Team: {} Stats'.format(team_name))
 		print('--------------------')
-		print('Total players: 6')
+		print('Total players: {}'.format(total_players))
 		print('')
 		print('Players on Team:')
-		print('    Karl Saygan, Chloe Alaska, Phillip Helm, Suzane Greenberg, Herschel Krustofski, Joe Smith')
+		players_string = ''
+		for name in team_roster[int(selection)-1][team_name]:
+			players_string += name['name'] + ', '
+		# print('    Karl Saygan, Chloe Alaska, Phillip Helm, Suzane Greenberg, Herschel Krustofski, Joe Smith')
+		print(players_string)
 		print('')
 		print('Press ENTER to continue...')
 
+	# Build teams
+	team_names  = constants.TEAMS
+	players     = constants.PLAYERS
+	team_roster = [{'Panthers':[]},{'Bandits':[]},{'Warriors':[]}]
+
+	for index, player in enumerate(players):
+		# Get index name of the team
+		team_name = list(team_roster[index % len(team_names)])[0]
+		# Add players evenly across the teams
+		team_roster[index % len(team_names)][team_name].append(player)
+
 	# main area
-
-	startup_message()
-	menu_selection()
+	# startup_message()
+	# menu_selection(1, 2)
 	list_teams()
-	menu_selection()
-	list_team()
+	# menu_selection(1, 2, 3)
+	# list_team()
 
-	
-
-
-	teams = constants.TEAMS
-	players = constants.PLAYERS
-	# print(teams)
-	# print(players)
 
 if __name__ == '__main__':
 	main()
