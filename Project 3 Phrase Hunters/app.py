7a5e61e43513e02d4c410ffa77f56e300b17b411
codebycody <codebycody@protonmail.com>
2020-04-20 20:59:19 -0400

    added files required for project 3

---
+++ Project 3 Phrase Hunters/app.py
@@ -0,0 +1,13 @@
+# pick a random phrase
+
+# player guesses leter
+
+# 	check if valid input
+# 		if not retry
+# 	Check if input was guessed before
+#		if guessed already retry
+# 	Check if in phrase
+# 		if in phrase reveal letter
+# 			if all letters revealed exit with winning message
+# 		else incroment wrong guess count by 1
+#			if wrong guess = 5 exit with losing message
